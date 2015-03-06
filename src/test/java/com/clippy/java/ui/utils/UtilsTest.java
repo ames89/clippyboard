9200c8c5c1d758185d039acf2a7c2419f4fed911
Angel Echavarria <aechavarria@spinplug.com>
2015-03-05 21:33:34 -0600

    adding tests

---
+++ src/test/java/com/clippy/java/ui/utils/UtilsTest.java
@@ -0,0 +1,14 @@
+package com.clippy.java.ui.utils;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UtilsTest extends TestCase {
+
+  @Test
+  public void testButtonCreateImg() throws Exception {
+
+  }
+}
